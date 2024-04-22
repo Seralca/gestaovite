6091fcac00737b724f3ab544802af9450825f839
Sergio Augusto Alcarria <sergioaugusto@correios.com.br>
2024-04-22 17:55:44 -0300

    Enviando projeto para deploy

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <p2>SG-Sistema de Gestão &copy; 2024</p2>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import './MyComponent.css'
+import imgOperacao from './img_Operacao.png'
+
+
+
+const MyComponents = () => {
+  return (
+    <div className="img1">
+      {/* <img src={imgOperacao} alt="Descrição da imagem" /> */} 
+    </div>
+  );
+}
+
+export default MyComponents
